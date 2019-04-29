20cf1b303902c61f195347e960ecf8cada4dcb25
Megan Adams <meganoliviawendyadams@gmail.com>
2019-04-29 06:23:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+<<<<<<< HEAD
 var greeting = "";
+=======
+var greeting = "Hello, everybody!";
+>>>>>>> 6007ed2d9603d674ab6be9b492c67ee7f9bcbaad
 
-var specialGuest = "Neil deGrasse Tyson"
+var specialGuest = "Neil deGrasse Tyson";
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `${topic}`;
+var conversation = "Let's talk about " + `${topic}` + ".";
